9279653e968c000b1b1d2722200f455170b8f606
“Sebastian <iscrozo@gmail.com>
2023-01-18 23:17:44 -0500

    update elements with view programmatically2

--- CatchPokemon/PokemonViewController.swift
+++ CatchPokemon/PokemonViewController.swift
@@ -8,12 +8,29 @@
 import UIKit
 
 class PokemonViewController: UIViewController {
-
+    
+    @IBOutlet weak var pokemonImage: UIImageView!
+    @IBOutlet weak var labelScore: UILabel!
+    @IBOutlet weak var labelMessage: UILabel!
+    @IBOutlet var answerButtons: [UIButton]!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        for button in answerButtons {
+            button.layer.shadowColor = UIColor(red: 0, green: 0, blue: 0, alpha: 0.5).cgColor
+            button.layer.shadowOffset = CGSize(width: 0, height: 2.0)
+            button.layer.shadowOpacity = 1
+            button.layer.shadowRadius = 0
+            button.layer.masksToBounds = false
+            button.layer.cornerRadius = 10.0
+        }
     }
 
-
+    
+    
+    @IBAction func buttonPressed(_ sender: UIButton) {
+    }
+    
 }
 
