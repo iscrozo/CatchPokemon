de9d29e859381975105e6096b99593b5b1025cf4
“Sebastian <iscrozo@gmail.com>
2023-01-26 08:16:05 -0500

    create a new file for viewController for the other storyboard, and create new segue for between 2 storyboards

---
+++ CatchPokemon/Controller/ResultViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ResultViewController.swift
+//  CatchPokemon
+//
+//  Created by Camilo Rozo on 26/01/23.
+//
+
+import UIKit
+
+class ResultViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+
+}
